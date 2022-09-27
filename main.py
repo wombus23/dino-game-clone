9d21bd8fe7f7b22183d1fd2ffe3bbda9d7f8672d
Noor Ejaz <noorejaz576@gmail.com>
2022-09-27 10:43:55 -0700

    engine.py was removed and relevant game character was placed on screen

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("game"":D")
+import sys
+import pygame
+
+pygame.init()
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 400
+done = False
+GAME_CLOCK = pygame.time.Clock()
+BACKGROUND_COLOR = (255, 255, 255)
+GAME_WINDOW = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
+ground_sprite = pygame.image.load("ground.png").convert_alpha()
+dino_sprite = pygame.image.load("dino.png").convert_alpha()
+
+
+pygame.display.set_caption("DINO CHROME RUNNER")
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.quit()
+            exit()
+        GAME_WINDOW.fill(BACKGROUND_COLOR)
+        GAME_WINDOW.blit(ground_sprite, (-10, 150))
+        pygame.display.flip()
+        GAME_WINDOW.blit(dino_sprite, (10, 180))
+        pygame.display.update()
+        GAME_CLOCK.tick(60)
+        print(GAME_CLOCK)
+
+
+
+
+
+
+
+
+
+
